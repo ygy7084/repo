27d24ea1180858f0f1af15357e4d02f399d5cf59
SC-201610290233\Administrator <1neptune1@naver.com>
2016-12-13 22:30:50 +0900

    c1

--- server/main.js
+++ server/main.js
@@ -8,9 +8,21 @@ import path from 'path';
 const app = express();
 const port = 3000;
 
+if(process.env.NODE_ENV == 'development') {
+    console.log('development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server'+devPort);
+        }
+    )
+}
+
 app.use('/', express.static(path.join(__dirname, './../public')));
 app.get('/hello', (req,res) => {
-    return res.send('Hello CodelAAab');
+    return res.send('Hello CodelAjAab');
 });
 app.listen(port, () => {
     console.log('server on',port);
